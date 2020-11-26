6c68b6e8c2b8f8eadd423acbc9c64622489e64a5
Stephen McKinney <stephen.mckinney@conferma.com>
2020-11-26 14:16:52 +0000

    initial commit

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,43 @@
+const Page = require('../pageobjects/page');
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+const HomePage = new Page();
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        LoginPage.open();
+
+        LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        expect(SecurePage.flashAlert).toBeExisting();
+        expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+    it('should get get text from element specified by tag', () => {
+        HomePage.open('');
+        let h2 = $('h2');
+        expect(h2).toHaveTextContaining(
+            'Examples'
+        );
+        console.log(`Text in h2 tags is ${h2.getText()}`);
+    });
+
+    it('should get get text from element specified by id', () => {
+        HomePage.open('');
+        let pageFooter = $('#page-footer');
+        expect(pageFooter).toHaveTextContaining(
+            'Elemental Selenium'
+        );
+        console.log(`Text in the element with id 'page-footer' is ${pageFooter.getText()}`);
+    });
+
+    it('should get get text from element specified by class', () => {
+        HomePage.open('');
+        let heading = $('.heading');
+        expect(heading).toHaveTextContaining(
+            'Welcome to the-internet'
+        );
+        console.log(`Text in the element with class 'header' is ${heading.getText()}`);
+    });
+});
+
+
